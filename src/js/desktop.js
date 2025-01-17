94fa670efae8da66bbfd9ced6f135b40e3080c6d
falya128 <falya128@users.noreply.github.com>
2025-01-18 00:49:19 +0900

    メッセージ送信機能の作成

--- src/js/desktop.js
+++ src/js/desktop.js
@@ -1,22 +1,86 @@
+import Swal from 'sweetalert2';
+
 (function (PLUGIN_ID) {
-  kintone.events.on('app.record.index.show', () => {
-    const spaceEl = kintone.app.getHeaderSpaceElement();
-    if (spaceEl === null) {
-      throw new Error('The header element is unavailable on this page.');
-    }
+  kintone.events.on('app.record.detail.show', () => {
+    const sendButton = document.createElement('button');
+    sendButton.textContent = 'メッセージ送信';
+    sendButton.className = 'kintoneplugin-button-dialog-ok';
+    sendButton.style = 'margin:16px 0 0 16px';
+    sendButton.onclick = showAlert;
 
-    const fragment = document.createDocumentFragment();
-    const headingEl = document.createElement('h3');
-    const messageEl = document.createElement('p');
-
-    const config = kintone.plugin.app.getConfig(PLUGIN_ID);
-    messageEl.textContent = config.message;
-    messageEl.classList.add('plugin-space-message');
-    headingEl.textContent = 'Hello kintone plugin!';
-    headingEl.classList.add('plugin-space-heading');
-
-    fragment.appendChild(headingEl);
-    fragment.appendChild(messageEl);
-    spaceEl.appendChild(fragment);
+    const headerMenuSpace = kintone.app.record.getHeaderMenuSpaceElement();
+    headerMenuSpace.appendChild(sendButton);
   });
+
+  /**
+   * メッセージ送信アラート表示
+   */
+  const showAlert = () => {
+    Swal.fire({
+      title: 'LINEメッセージ送信',
+      text: '送信するメッセージを入力してください。',
+      input: 'textarea',
+      showCancelButton: true,
+      cancelButtonText: 'キャンセル',
+      confirmButtonText: '送信',
+      preConfirm: async (message) => {
+        // 入力チェック
+        if (message === '') {
+          Swal.showValidationMessage('メッセージを入力してください。');
+          return;
+        }
+        try {
+          const { record } = kintone.app.record.get();
+          const userId = record['LINEユーザーID'].value;
+          await sendMessage(userId, message);
+          await createHistory(userId, message);
+        } catch (error) {
+          console.error(error);
+          Swal.showValidationMessage('メッセージの送信に失敗しました。');
+        }
+      },
+      allowOutsideClick: () => !Swal.isLoading(),
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: '送信完了',
+          text: 'メッセージが送信されました。',
+          icon: 'success',
+        }).then(() => location.reload());
+      }
+    });
+  };
+
+  /**
+   * LINEにメッセージ送信
+   */
+  const sendMessage = async (userId, message) => {
+    const [, status] = await kintone.plugin.app.proxy(
+      PLUGIN_ID,
+      'https://api.line.me/v2/bot/message/push',
+      'POST',
+      {},
+      {
+        to: userId,
+        messages: [{ type: 'text', text: message }],
+      },
+    );
+    if (status !== 200) {
+      throw new Error('メッセージの送信に失敗しました。');
+    }
+  };
+
+  /**
+   * メッセージ履歴の登録
+   */
+  const createHistory = async (userId, message) => {
+    const appId = kintone.app.getRelatedRecordsTargetAppId('メッセージ履歴');
+    await kintone.api(kintone.api.url('/k/v1/record.json', true), 'POST', {
+      app: appId,
+      record: {
+        LINEユーザーID: { value: userId },
+        送信メッセージ: { value: message },
+      },
+    });
+  };
 })(kintone.$PLUGIN_ID);
